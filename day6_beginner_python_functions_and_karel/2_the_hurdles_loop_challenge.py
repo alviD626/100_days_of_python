90fdc871f9b94081ed0bb6c061e396fefd18f5c3
alvi <alvi@nitex.info>
2024-10-29 09:22:38 +0600

    add 1st 2 program

---
+++ day6_beginner_python_functions_and_karel/2_the_hurdles_loop_challenge.py
@@ -0,0 +1,31 @@
+"""
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def move_forward():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    
+def make_rotation():    
+    move_forward()
+    turn_left()
+    move_forward()
+    turn_left()
+    move_forward()
+    turn_left()
+    move_forward()
+    turn_left()
+    move_forward()
+    turn_left()
+    
+    
+make_rotation()
+move_forward()
+"""
