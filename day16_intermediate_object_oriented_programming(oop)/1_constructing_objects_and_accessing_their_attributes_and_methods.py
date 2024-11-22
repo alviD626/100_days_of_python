cf68d583f057a3a70ab8668629bf6e8791a4a82e
alvi <alvi@nitex.com>
2024-11-21 17:49:24 -0800

    add day16 half exercise

---
+++ day16_intermediate_object_oriented_programming(oop)/1_constructing_objects_and_accessing_their_attributes_and_methods.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+timmy = Turtle()
+print(timmy)
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+timmy.left(120)
+timmy.forward(100)
+timmy.left(120)
+timmy.forward(100)
+
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
---
+++ day16_intermediate_object_oriented_programming(oop)/3_quiz10_python_objects_quiz.py
@@ -0,0 +1,56 @@
+"""
+Question 1:
+In OOP what is the name of the blueprint for creating objects?
+
+1. Attribute
+2. Method
+3. Class
+4. Instance
+
+Ans: 3. Class
+
+
+"""
+
+
+"""
+Question 2:
+Given a Class blueprint for a Car has the following attributes and methods, which line of code in the answers will produce an error?
+
+Attributes:
+
+num_of_seats
+
+speed
+
+Methods:
+
+drive()
+
+brake()
+
+1. car.drive()
+2. car.num_of_seats = 2
+3. car.brake() = 0
+4. print(car.speed)
+
+Ans: 3. car.brake() = 0
+
+"""
+
+
+"""
+Question 3:
+my_toyota = Car()
+my_fiat = Car()
+What word would you use to describe what's inside my_toyota and my_fiat?
+
+1. Class
+2. Attribute
+3. Variable
+4. Object
+5. Method
+
+Ans: 4. Object
+
+"""
